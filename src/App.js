8a6565793ecd1832beef1024b23c712ce7a32779
sinachi ajuzie <Ajuziesinachi@gmail.com>
2023-08-25 22:49:11 +0100

    structure of the application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import './styles/Homepage.css';
+import {  } from "module";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+      <body>
+        <div className="container">
+        <div className="col-xs-12">
+          {/* export these styles */}
+                <div className="home-text-padding">
+                    <h2 className="home-text">ENSLinkTree</h2>
+                    <p>
+                      Use your ENS name as a link tree to get discovered
+                    </p>
+                    <p>
+                      You can also receive tips and donations to extend your ENS name ownership
+                    </p>
+                    <a href='./createLink'>
+                      <button>
+                      Get Started
+                      </button>
+                    </a>
+                </div>
+        </div>
+        </div>
+      </body>
   );
 }
 
